dad6b466fd90c68840e4c5831e8c7ea5454e884c
Elva Su <112391401+HWenSu@users.noreply.github.com>
2024-07-30 21:43:33 +0800

    Recreate localImageUploader

--- src/components/LocalImageUploader.js
+++ src/components/LocalImageUploader.js
@@ -4,28 +4,54 @@ const LocalImageUploader = () => {
 
   const [selected, setSelected] = useState(null)
   const [previewURL, setPreviewURL] = useState(null)
-  const validFiles = []
+  
   
   // 選取圖片時
   const handleFileChange = (e) => {
     const files = Array.from(e.target.files);
+    const data = [];
 
     files.forEach((file) => {
-      validFiles.push(file)
+      const reader = new FileReader()
+      reader.onload = function (event) {
+        const image = new Image()
+        image.src = event.target.result // 用 FileReader 讀取的 URL
+        image.onload = function() {
+          const width = image.width
+          const height = image.height
+          const url = image.src
+          //用物件封裝
+          const imageData = {
+            'width': width,
+            'height': height,
+            "src": {
+              "large": url,
+            }
+          };
+        data.push(imageData)
+        // 確保所有文件都處理完 再更新狀態
+        if (data.length === files.length) {
+          setSelected(data)
+        }
+        }
+      }
+      reader.readAsDataURL(file) // 預覽圖片
     })
-    setSelected(validFiles);
   }
 
  useEffect(() => {
    // 為每個文件創建預覽 URL
    if(selected) {
-   const urls = selected.map((file) => URL.createObjectURL(file));
-   setPreviewURL(urls);
-   localStorage.setItem('imageURLs', JSON.stringify(urls))
+    //讀取現有的URL
+    const existingURLs = JSON.parse(localStorage.getItem("imageURLs")) || [];
+    //將新的URL新增到現有的URL陣列中
+    const newURLs = selected.map((file) => file.src.large);
+    const updateURLs = [...existingURLs, ...newURLs];
+    //儲存至Local storage
+    localStorage.setItem('imageURLs', JSON.stringify(updateURLs))
    }
  }, [selected]);
 
-  
   return (
     <div>
       <input
@@ -36,12 +62,9 @@ const LocalImageUploader = () => {
       />
       <div class="previewContainer">
         {selected && selected.map((file, index) => {
-          const previewURL = URL.createObjectURL(file);
-          
           return (
-            <div class="imageContainer">
-              <img src={previewURL} alt="preview" />
-              {}
+            <div class="imageContainer" key={index}>
+              <img src={file.src.large} alt="preview" />
             </div>
           );
         })}
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import Waterfall from '../components/Waterfall'
 
-
-
-
+const dataArr = localStorage.getItem('imageURLs')
 
 const Portfolio = () => {
   return (
     <div>
-
+      {dataArr && <Waterfall data={dataArr} width={window.innerWidth} />
+      }
     </div>
   );
 }
