aee3e64d901fe880b6741a72f503914442cfeb2b
Elva Su <112391401+HWenSu@users.noreply.github.com>
2025-02-13 22:54:08 +0800

    Stye: Favorite page loading UI

--- src/pages/Favorite.js
+++ src/pages/Favorite.js
@@ -10,27 +10,47 @@ const Favorite = () => {
   const {user} = useAuth()
   const [favorites, setFavorites]=useState([])
   const [isAlert, setIsAlert]=useState(false)
+  const [isLoading, setIsLoading]=useState(true)
 
   useEffect(()=>{
-        if(!user){
-        setIsAlert(true)
-        setTimeout(()=>{setIsAlert(false)}, 1500)
-        //如果 user 還未初始化，直接返回，等待下一次渲染
-        return
-      }
-    const fetchFavorites = async ()=> {
-      const querySnapshot = await getDocs(collection(db,`users/${user.uid}/favorites`))
-      setFavorites(querySnapshot.docs.map((doc)=>doc.data()))
+    // user 還沒載入時，直接返回，不做任何操作
+    if (user === undefined) return;
+    // 沒有登入的情況
+    if (!user) {
+      setIsAlert(true);
+      setTimeout(() => setIsAlert(false), 1500);
+      setTimeout(() => setIsLoading(false), 500); // 防止 isLoading 太快消失，導致畫面閃爍
+      //如果 user 還未初始化，直接返回，等待下一次渲染
+      return;
     }
-    fetchFavorites()
-  }, [user, favorites])// 當 user 初始化完成後，重新執行 useEffect
 
+    const fetchFavorites = async () => {
+      setIsLoading(true)
+      try{
+        const querySnapshot = await getDocs(
+        collection(db, `users/${user.uid}/favorites`)
+        );
+        setFavorites(querySnapshot.docs.map((doc) => doc.data()));
+      } catch(error){
+        console.error('Error fetching favorites:', error)
+      }
+      setIsLoading(false);
+    };
+  fetchFavorites();
+  }, [user]) // 當 user 初始化完成後，重新執行 useEffect
 
-  return (
-    user?(
+  //先判斷是否是 isLoading 
+  if(isLoading){
+    return (
+      <div className="loading-container">
+        <p>Loading...</p>
+      </div>
+    )
+  }
+  //再檢查是否登入
+  return user?(
     <div className='favorite-container'>
       <Waterfall
-      // imgURL={imgURL()}
       data={favorites}
       width={window.innerWidth}
       />
@@ -39,7 +59,7 @@ const Favorite = () => {
       {isAlert&& <AlertModal content={'Please login to access Favorite'}/>} 
      <Login/>
     </div>
-  ))
+  )
 }
 
 export default Favorite
