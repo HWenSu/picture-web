6f6681fe6938c302a5fafe80cc314a9a6db4d165
HWenSu <trista44488@gmail.com>
2025-02-15 17:39:37 +0800

    Style: mobile share link modal and mobile alert modal

--- src/components/PictureModal.js
+++ src/components/PictureModal.js
@@ -1,9 +1,9 @@
-import { useAuth } from "../context/AuthContext"
 import { useState } from "react"
 import Collect from "./Collect"
+import DownloadImage from "./DownloadImage"
+import ShareURL from "./ShareURL"
 
 const PictureModal = ( { data, src, alt, photographer, photographer_url, url, handlePicture, isFavorite, onFavoriteChange } ) => {
-  const {user} = useAuth()
   const [isClosed, setIsClosed]=useState(false)
   console.log(data)
   const title = data.title
@@ -26,15 +26,23 @@ const PictureModal = ( { data, src, alt, photographer, photographer_url, url, ha
           <h2>{photographer?.toUpperCase()}</h2>
         </div>
         {photographer&&
+        <>
         <div className="btn-container">
+          {/* 下載圖片按紐   */}
+          <div className='download-btn'>
+            <DownloadImage imgURL={src} fileName={data.id} />
+          </div>
+          {/* 收藏按鈕 */}
           <Collect data={data} isFavorite={isFavorite} onFavoriteChange={onFavoriteChange} />
-          <button className="main-button">
+        </div>
+       
+        <button className="main-button">
           <a href={url} target="_blank" rel="noreferrer">
           Visit Website
           </a>
-          </button>
-        </div>
-        }
+        </button>
+        </>
+         }
         {title&&
         <div>
           {console.log(data)}
